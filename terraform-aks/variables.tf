f82efff9b600bf90fce07d2ab512fa69ca958996
Pratyushaa94 <prathyusha9449@gmail.com>
2025-08-13 17:05:30 +0530

    added provider

--- terraform-aks/variables.tf
+++ terraform-aks/variables.tf
@@ -42,3 +42,24 @@ variable "node_vm_size" {
   type        = string
   default     = "Standard_DS2_v2"
 }
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client (Application) ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
